cb400e7d4209d836b3183880362346fbaff38c8c
anweshaghosh12345 <61232824+anweshaghosh12345@users.noreply.github.com>
2023-04-05 19:39:07 +0530

    AR camera functionality

--- lib/Screens/video_screen.dart
+++ lib/Screens/video_screen.dart
@@ -1,4 +1,4 @@
-import 'package:better_player/better_player.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
